4310e10a3fdf92a7896751ce183b013b416ef110
Afshin Arani <afshin@arani.dev>
2022-11-09 14:18:55 +0330

    Update package org to nblockchain

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-	"github.com/vultr/terraform-provider-vultr/vultr"
+	"github.com/nblockchain/terraform-provider-vultr/vultr"
 )
 
 func main() {
